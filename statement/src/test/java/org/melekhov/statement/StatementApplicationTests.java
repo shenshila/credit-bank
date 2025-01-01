dec0cc77b8020573192869063034ae94115b7ce5
Melekhov Andrey <cool.melekhov@gmail.com>
2025-01-01 17:00:52 +0400

    create liquibase

---
+++ statement/src/test/java/org/melekhov/statement/StatementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.melekhov.statement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
